65e34e65ae81c5df174079b283412b0e08b8c823
viktoryboykova <viktoryboykova@list.ru>
2022-03-02 16:20:27 +0300

    Что такое интеграционное тестирование

---
+++ src/main/java/ru/job4j/integration/OrdersStore.java
@@ -0,0 +1,116 @@
+package ru.job4j.integration;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class OrdersStore {
+    private final BasicDataSource pool;
+
+    public OrdersStore(BasicDataSource pool) {
+        this.pool = pool;
+    }
+
+    public Order save(Order order) {
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement(
+                     "INSERT INTO orders(name, description, created) VALUES (?, ?, ?)",
+                     PreparedStatement.RETURN_GENERATED_KEYS)) {
+            pr.setString(1, order.getName());
+            pr.setString(2, order.getDescription());
+            pr.setTimestamp(3, order.getCreated());
+            pr.execute();
+            ResultSet id = pr.getGeneratedKeys();
+            if (id.next()) {
+                order.setId(id.getInt(1));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return order;
+    }
+
+    public Collection<Order> findAll() {
+        List<Order> rsl = new ArrayList<>();
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("SELECT * FROM orders")) {
+            try (ResultSet rs = pr.executeQuery()) {
+                while (rs.next()) {
+                    rsl.add(
+                            new Order(
+                                    rs.getInt("id"),
+                                    rs.getString("name"),
+                                    rs.getString("description"),
+                                    rs.getTimestamp(4)
+                            )
+                    );
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public Order findById(int id) {
+        Order rsl = null;
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("SELECT * FROM orders WHERE id = ?")) {
+            pr.setInt(1, id);
+            ResultSet rs = pr.executeQuery();
+            if (rs.next()) {
+                rsl = new Order(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getTimestamp(4)
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public Order findByName(String name) {
+        Order rsl = null;
+        try (Connection con = pool.getConnection();
+             PreparedStatement pr = con.prepareStatement("SELECT * FROM orders WHERE name = ?")) {
+            pr.setString(1, name);
+            ResultSet rs = pr.executeQuery();
+            if (rs.next()) {
+                rsl = new Order(
+                        rs.getInt("id"),
+                        rs.getString("name"),
+                        rs.getString("description"),
+                        rs.getTimestamp(4)
+                );
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rsl;
+    }
+
+    public boolean update(Order order) {
+        boolean result = false;
+        try (Connection con = pool.getConnection();
+             PreparedStatement statement =
+                     con.prepareStatement("UPDATE orders SET name = ?, description = ? WHERE id = ?")) {
+            statement.setString(1, order.getName());
+            statement.setString(2, order.getDescription());
+            statement.setInt(3, order.getId());
+            statement.execute();
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
